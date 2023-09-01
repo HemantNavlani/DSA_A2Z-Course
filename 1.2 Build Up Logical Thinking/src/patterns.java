5d36a12e843e7f37a1315b306609dfe09c31b422
HemantNavlani <hemant.navlani.0506@gmail.com>
2023-09-01 15:22:58 +0530

    Patterns

---
+++ 1.2 Build Up Logical Thinking/src/patterns.java
@@ -0,0 +1,4 @@
+public class patterns {
+    //The patterns are mainly for logical building these are not asked in interviews directly. I have done it one or two times some different patterns so I am not doing patterns question here as I want to practice questions of the main topics as soon as possible.
+    
+}
--- 1.Learn the Basics/1.1 Language Basics/src/TimeandSpaceComplexity.java
+++ 1.Learn the Basics/1.1 Language Basics/src/TimeandSpaceComplexity.java
@@ -2,5 +2,5 @@ public class TimeandSpaceComplexity {
 
     // ye video dekh lena ek baar
     //https://youtu.be/FPu9Uld7W-E?si=JQA3VkOyivFnmutz
-    
+
 }
